5b6c92cf44d153d5a54403c3ac6a3c7e5bdbc219
kennedyb <brian.kennedy@wwt.com>
2017-04-10 14:48:40 -0500

    removing passwords

--- boxConfig.js
+++ boxConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-  clientId: "2poe48vnmyhlokdy9t2mjbty4bcrwlo0",
-  clientSecret: "EjJiS85ut6ndAXZuWHyZSx6JtsooPtUx",
-  enterpriseId: "13192158",
-  jwtPrivateKeyPassword: "Pass123!",
-  jwtPublicKeyId: "cbfzxxts"
+  clientId: "",
+  clientSecret: "",
+  enterpriseId: "",
+  jwtPrivateKeyPassword: "",
+  jwtPublicKeyId: ""
 }
