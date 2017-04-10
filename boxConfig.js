ae6a2f42c2d7d7f671a46e78d34d12c01d2c7ffe
kennedyb <brian.kennedy@wwt.com>
2017-04-10 14:36:31 -0500

    updating with openssl-created public and private keys

--- boxConfig.js
+++ boxConfig.js
@@ -2,6 +2,6 @@ module.exports = {
   clientId: "2poe48vnmyhlokdy9t2mjbty4bcrwlo0",
   clientSecret: "EjJiS85ut6ndAXZuWHyZSx6JtsooPtUx",
   enterpriseId: "13192158",
-  jwtPrivateKeyPassword: "password",
-  jwtPublicKeyId: "5lk85n0q"
+  jwtPrivateKeyPassword: "Pass123!",
+  jwtPublicKeyId: "cbfzxxts"
 }
